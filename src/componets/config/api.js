99f8a32240fbae78363676311ce13d71a23f7fa2
surajit <samantasurajit@533.com>
2026-05-16 07:18:17 +0530

    all react done

--- src/componets/config/api.js
+++ src/componets/config/api.js
@@ -1,20 +1,29 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://social-app-backend-production-c81c.up.railway.app";
+export const API_BASE_URL =
+  "https://social-app-backend-production-c81c.up.railway.app";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
+  withCredentials: true,
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("jwt");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+api.interceptors.request.use(
+  (config) => {
+
+    const token = localStorage.getItem("jwt");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
