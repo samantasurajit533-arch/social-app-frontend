0a269e648b1476b460606f89a3ba179f54ea72ec
surajit <samantasurajit@533.com>
2026-05-25 20:10:10 +0530

    create all table aii

--- src/componets/Post/PostCard.jsx
+++ src/componets/Post/PostCard.jsx
@@ -11,7 +11,6 @@ import BookmarkIcon from '@mui/icons-material/Bookmark';
 import { useDispatch, useSelector } from 'react-redux';
 import { createCommentAction, likePostAction } from '../../pages/Redux/Post/post.action';
 
-import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { Box, Avatar, Typography, Card } from '@mui/material'; // Adjust imports based on your structure
